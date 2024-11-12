6bd9eb7f97361728948fb68162d1ba78bd3b413a
rushi818 <rushikeshbhoi33@gmail.com>
2024-11-13 00:04:36 +0530

    rushi commit 2

--- src/main/java/com/velocity/rest/CustomerRestController.java
+++ src/main/java/com/velocity/rest/CustomerRestController.java
@@ -25,6 +25,7 @@ public class CustomerRestController {
 		
 		System.out.println("Hello");
 		System.out.println("hello everyone");
+		System.out.println("Good Morning...");
 		System.out.println("hello controller");
 		return customer;
 		
